7b577fd1fae04a8e11e1e34894f54be4f981c558
qauinger <92843977+qauinger@users.noreply.github.com>
2023-05-23 10:28:21 -0400

    Add student layout

--- src/app/(students)/layout.tsx
+++ src/app/(students)/layout.tsx
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google'
-import { Header } from '@components/Header'
+import { UnlinkedHeader as Header } from '@components/Header'
 import '@css/theme.css';
 
 const inter = Inter({ subsets: ['latin'] })
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,17 @@
-export default function Header() {
+function Header() {
     return(
         <div id="header">
             <a href="https://qauinger.com/brainwave"><img src='/img/icon.svg' alt="Brainwave"/>Brainwave</a>
         </div>
     );
 }
+
+function UnlinkedHeader() {
+    return(
+        <div id="header">
+            <a><img src='/img/icon.svg' alt="Brainwave"/>Brainwave</a>
+        </div>
+    );
+}
+
+export {Header, UnlinkedHeader}; 
